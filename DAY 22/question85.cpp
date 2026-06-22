eb4ba48871fd5df533d3ace091aa3514325aef2c
ABHAYADAV5856 <abhayadav5856@gmail.com>
2026-06-22 22:33:32 +0530

    Added DAY 22 Assignment in C++

---
+++ DAY 22/question85.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+int main() {
+    char str[100];
+    int i, len, flag = 1;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    len = strlen(str);
+
+    for(i = 0; i < len / 2; i++) {
+        if(str[i] != str[len - i - 1]) {
+            flag = 0;
+            break;
+        }
+    }
+
+    if(flag)
+        printf("Palindrome String\n");
+    else
+        printf("Not a Palindrome String\n");
+
+    return 0;
+}
